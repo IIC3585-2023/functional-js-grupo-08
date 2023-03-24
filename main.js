633d04d6719f08433f057f9c867357d88a54e296
Guillermo Achondo <guillermo.achondo@uc.cl>
2023-03-24 17:36:11 -0300

    started input erro handling

--- main.js
+++ main.js
@@ -2,5 +2,14 @@
 import { ProcessMarkdownFile} from "./process_markdown.js";
 
 const args = process.argv;
-ProcessMarkdownFile(args[2], args[3]);
+
+
+if (args[2] === undefined) {
+    console.log("Especifique la ruta del archivo Markdown")
+} else if (args[3] === undefined) {
+    console.log("Especifique el nombre del archivo HTML")
+} else {
+    ProcessMarkdownFile(args[2], args[3]);
+}
+
 
