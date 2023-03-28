c199ff7fc04c8a9f7dab8a74a26b245aa4735289
Tomas <tconcha@uc.cl>
2023-03-27 21:40:28 -0300

    old pr, to new format

--- process_lines.js
+++ process_lines.js
@@ -1,13 +1,5 @@
 // Función por liena (recibe una linea y devuelve una liena)
 const convertLine = (line) => {
-
-    // Negrita
-    const line = line
-        .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
-        .replace(/__(.*?)__/g, '<strong>$1</strong>')
-        .replace(/\*(.*?)\*/g, '<em>$1</em>')
-        .replace(/_(.*?)_/g, '<em>$1</em>');
-
     /*
     //Enlaces
     line = line.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
@@ -20,7 +12,7 @@ const convertLine = (line) => {
     if (line.match(/^---$/)) {
         return '<hr>\n';
     }
-
+    line = processText(line);
     let counter =header(line,0); //  ###3 => va a desaparecer el 3
     if(counter>0){
       return `<h${counter}>${line.slice(counter+1)}</h${counter}>\n`;
@@ -33,9 +25,18 @@ const convertLine = (line) => {
 //Funcion para contar #
 let header = (string, counter) => string[counter]==="#"?header(string,counter+1):counter;
 
+//Funcion para procesar texto
+const processText = (text) => {
+    return text
+    .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+    .replace(/__(.*?)__/g, '<strong>$1</strong>')
+    .replace(/\*(.*?)\*/g, '<em>$1</em>')
+    .replace(/_(.*?)_/g, '<em>$1</em>');
+}
+
 //Funcion para procesar una lista
 const processList = (lines) => {
-    lines = lines.map(line => {return line.replace('* ', '<li>') + '</li>\n'});
+    lines = lines.map(line => {return processText(line.replace('* ', `   <li>`) + '</li>\n');});
 
     lines.push("</ul>\n"); // agregar tabs en lo que imprime
     lines.unshift("<ul>\n");
@@ -47,7 +48,7 @@ const processOrderdList = (lines) => {
     let counter = 0;
     lines = lines.map(line => {
       counter++;
-      return line.replace(`${counter}. `, '<li>') + '</li>\n'
+      return processText(line.replace(`${counter}. `, `   <li>`) + '</li>\n');
     });
     lines.push("</ol>\n");
     lines.unshift("<ol>\n");
